7bc2fb4b63c900671311342e5e9c4236f45bff64
@LuizCarlosAbbott <luizcarlos_abbott@hotmail.com>
2020-03-30 23:01:40 -0300

    Adding update deploy

--- src/components/template/Footer.jsx
+++ src/components/template/Footer.jsx
@@ -1,13 +1,18 @@
-import './Footer.css'
-import React from 'react'
+import "./Footer.css";
+import React from "react";
 
-export default props =>
-    <footer className="footer">
-        <span>
-            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
-            <strong> Cod<span className="text-danger">3</span>r</strong>
-        </span>
-    </footer>
+export default props => (
+  <footer className="footer">
+    <span>
+      Desenvolvido com <i className="fa fa-heart text-danger"></i> por Luiz
+      seguindo instruções da
+      <strong>
+        {" "}
+        Cod<span className="text-danger">3</span>r
+      </strong>
+    </span>
+  </footer>
+);
 
 // fa fa-heart é um coração do fontes-awesome
-// text-danger para o nome ficar vermelho    
+// text-danger para o nome ficar vermelho
